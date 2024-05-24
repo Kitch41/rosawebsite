296a9ecd5aef98b1d4c3d1bdb1e02e23dd39cecc
Stef Keuken <91873549+Kitch41@users.noreply.github.com>
2024-05-24 16:44:50 +0200

    added swipers

---
+++ js/swiper.js
@@ -0,0 +1,52 @@
+const swiper = new Swiper(".swiper", {
+    direction: "horizontal",
+    loop: true,
+    speed: 1500,
+    slidesPerView: 5,
+    spaceBetween: 60,
+    mousewheel: false,
+    parallax: false,
+    centeredSlides: false,
+    effect: "coverflow",
+    coverflowEffect: {
+      rotate: -5,
+      slideShadows: true
+    },
+    autoplay: {
+      delay: 2000,
+      pauseOnMouseEnter: true
+    },
+    pagination: {
+        el: ".swiper-pagination",
+      },
+    scrollbar: {
+      el: ".swiper-scrollbar"
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 2,
+        spaceBetween: 60
+      },
+      600: {
+        slidesPerView: 3,
+        spaceBetween: 60
+      },
+      1000: {
+        slidesPerView: 4,
+        spaceBetween: 60
+      },
+      1400: {
+        slidesPerView: 5,
+        spaceBetween: 60
+      },
+      2300: {
+        slidesPerView: 5,
+        spaceBetween: 60
+      },
+      2900: {
+        slidesPerView: 6,
+        spaceBetween: 60
+      }
+    }
+  });
+  
