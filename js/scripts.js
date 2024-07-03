32b9df8bcd36aaaf15a3ca356068f74ee09f5c11
Kitch41 <stefkeuken@hotmail.com>
2024-07-03 19:32:20 +0200

    latest

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,75 @@
-console.log("heya")
+document.addEventListener('DOMContentLoaded', function() {
+  console.log("heya");
 
-const hamburger = document.querySelector('.hamburger');
-const navLink = document.querySelector('.nav__link');
+  const hamburger = document.querySelector('.hamburger');
+  const navLinks = document.querySelectorAll('.nav__link');
 
-hamburger.addEventListener('click', () => {
-  navLink.classList.toggle('hide');
+  hamburger.addEventListener('click', () => {
+    togglemenu();
+  });
+
+  // Get all video elements on the page
+  var videos = document.querySelectorAll('video');
+
+  // Loop through each video element
+  videos.forEach(function(video) {
+    // Set the volume to 0.5
+    video.volume = 0.5;
+  });
+
+  let prevScrollpos = window.pageYOffset;
+  window.onscroll = function() {
+    let currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+      document.getElementById("header").style.top = "0";
+    } else {
+      document.getElementById("header").style.top = "-100px"; /* Adjust this value to hide the header completely */
+      // Check if it's mobile
+      if (window.innerWidth <= 768) {
+        navLinks.forEach(function(navLink) {
+          if (!navLink.classList.contains('hide')) {
+            navLink.classList.toggle('hide');
+          }
+        });
+      }
+    }
+    prevScrollpos = currentScrollPos;
+  };
+
+  function togglemenu() {
+    navLinks.forEach(function(navLink) {
+      navLink.classList.toggle('hide');
+    });
+  }
+
+
+    // Get the current year
+    const currentYear = new Date().getFullYear();
+  
+    // Find elements with the placeholder
+    const elementsWithPlaceholder = document.querySelectorAll('[data-replace-year="year"]');
+  
+    // Replace the placeholder with the current year
+    elementsWithPlaceholder.forEach(function(element) {
+      element.textContent = currentYear;
+    });
+
+
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.intersectionRatio > 0) {
+          const duration = parseFloat(entry.target.dataset.pop) || 0.5; // Default duration is 0.5s
+          entry.target.style.transition = `opacity ${duration}s ease-in-out, transform ${duration}s ease-in-out`;
+          entry.target.classList.add('popped');
+          observer.unobserve(entry.target);
+        }
+      });
+    });
+  
+    const popTextElements = document.querySelectorAll('.pop-text');
+    popTextElements.forEach(element => {
+      observer.observe(element);
+    });
+  
+
 });
--- js/swiper.js
+++ js/swiper.js
@@ -1,49 +1,53 @@
 const swiper = new Swiper(".swiper", {
-    direction: "horizontal",
-    loop: true,
-    speed: 1500,
-    slidesPerView: 5,
-    spaceBetween: 60,
-    mousewheel: false,
-    parallax: false,
-    centeredSlides: false,
-    effect: "coverflow",
-    coverflowEffect: {
-      rotate: 10,
-      slideShadows: true
+  direction: "horizontal",
+  loop: true,
+  speed: 1500,
+  slidesPerView: 5,
+  spaceBetween: 60,
+  mousewheel: false,
+  parallax: false,
+  centeredSlides: true,
+  effect: "coverflow",
+  coverflowEffect: {
+    rotate: 10,
+    slideShadows: true
+  },
+  autoplay: {
+    delay: 2000,
+    pauseOnMouseEnter: true
+  },
+  pagination: {
+      el: ".swiper-pagination",
     },
-    autoplay: {
-      delay: 2000,
-      pauseOnMouseEnter: true
+  loopAdditionalSlides: 1, // Increase the number of additional slides
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 60
     },
-    pagination: {
-        el: ".swiper-pagination",
-      },
-    breakpoints: {
-      0: {
-        slidesPerView: 2,
-        spaceBetween: 60
-      },
-      600: {
-        slidesPerView: 3,
-        spaceBetween: 60
-      },
-      1000: {
-        slidesPerView: 4,
-        spaceBetween: 60
-      },
-      1400: {
-        slidesPerView: 5,
-        spaceBetween: 60
-      },
-      2300: {
-        slidesPerView: 5,
-        spaceBetween: 60
-      },
-      2900: {
-        slidesPerView: 6,
-        spaceBetween: 60
-      }
+    400: {
+      slidesPerView: 2,
+      spaceBetween: 60
+    },
+    600: {
+      slidesPerView: 3,
+      spaceBetween: 60
+    },
+    1000: {
+      slidesPerView: 4,
+      spaceBetween: 60
+    },
+    1400: {
+      slidesPerView: 4,
+      spaceBetween: 60
+    },
+    2300: {
+      slidesPerView: 4,
+      spaceBetween: 60
+    },
+    2900: {
+      slidesPerView: 4,
+      spaceBetween: 60
     }
-  });
-  
+  }
+});
